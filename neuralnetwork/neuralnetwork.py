da6a6d93c45c85f8b1d12516e379cd512f28de05
ngabryluk <ncgabryluk@gmail.com>
2022-12-04 20:55:00 -0500

    Loaded data
    Used numpy's loadtxt function to load the data from our generated data and label text files.

--- neuralnetwork/neuralnetwork.py
+++ neuralnetwork/neuralnetwork.py
@@ -1,3 +1,24 @@
 # neuralnetwork
 """Predict Spotify song popularity based on numerous parameters using a neural network."""
 
+import numpy as np
+import os
+import pdb
+
+ROOT = os.path.dirname(os.path.dirname(__file__)) # Root directory of this code
+
+def main():
+    # Get the relevant files
+    datafile = os.path.expanduser(os.path.join(ROOT, "data", "data.txt"))
+    labelfile = os.path.expanduser(os.path.join(ROOT, "data", "labels.txt"))
+    # attributefile = os.path.expanduser(os.path.join(ROOT, "data", "attributes.txt"))
+
+    # Load the data from these files
+    x = np.loadtxt(datafile, delimiter=" ", ndmin=2)
+    t = np.loadtxt(labelfile, dtype=int)
+
+    pdb.set_trace()
+
+if __name__ == "__main__":
+    main()
+    
