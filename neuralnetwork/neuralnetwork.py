ece424ec2c5a4adbea0cbabac3c415efba79469a
ngabryluk <ncgabryluk@gmail.com>
2022-12-08 00:16:59 -0500

    Split data and Created Neural Network
    Split the data by allocating the first 75% of the data as training data and the remaining 25% as testing data. Also created an initial neural network.

--- neuralnetwork/neuralnetwork.py
+++ neuralnetwork/neuralnetwork.py
@@ -4,6 +4,8 @@
 import numpy as np
 import os
 import pdb
+from keras.models import Sequential
+from keras.layers import Input, Dense
 
 ROOT = os.path.dirname(os.path.dirname(__file__)) # Root directory of this code
 
@@ -13,9 +15,24 @@ def main():
     labelfile = os.path.expanduser(os.path.join(ROOT, "data", "labels.txt"))
     # attributefile = os.path.expanduser(os.path.join(ROOT, "data", "attributes.txt"))
 
-    # Load the data from these files
+    # Load the data from these files and split them into training and testing
     x = np.loadtxt(datafile, delimiter=" ", ndmin=2)
-    t = np.loadtxt(labelfile, dtype=int)
+    train_test_split = int(0.75 * len(x))
+    xtrain, xtest = x[:train_test_split, :], x[train_test_split:, :]
+    y = np.loadtxt(labelfile, dtype=int)
+    ytrain, ytest = y[:train_test_split], y[train_test_split:]
+    numSamples, numInputs = xtrain.shape
+    numOutputs = len(np.unique(ytrain))
+
+    pdb.set_trace()
+
+    # Create the neural network
+    model = Sequential()
+    model.add(Input(numInputs,))
+    model.add(Dense(units=500, activation='relu', name='hidden1'))
+    model.add(Dense(units=500, activation='relu', name='hidden2'))
+    model.add(Dense(units=numOutputs, activation='softmax', name='output'))
+    model.summary()
 
     pdb.set_trace()
 
