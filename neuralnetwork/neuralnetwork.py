be0fa86ac57e76b0d12189259b666b97a1eb4463
ngabryluk <ncgabryluk@gmail.com>
2022-12-09 21:11:09 -0500

    Add code for accuracy and plotting the performance
    Using the evaluate method to get the loss and accuracy of the training and testing data and using pyplot to show the performance as the model was being trained.

--- neuralnetwork/neuralnetwork.py
+++ neuralnetwork/neuralnetwork.py
@@ -4,10 +4,12 @@
 import numpy as np
 import os
 import pdb
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Input, Dense
 from keras.optimizers import SGD
 from keras.callbacks import EarlyStopping
+from keras.utils import to_categorical
 
 ROOT = os.path.dirname(os.path.dirname(__file__)) # Root directory of this code
 
@@ -20,27 +22,30 @@ def main():
     # Load the data from these files and split them into training and testing
     x = np.loadtxt(datafile, delimiter=" ", ndmin=2)
     train_test_split = int(0.75 * len(x))
-    xtrain, xtest = x[:train_test_split, :], x[train_test_split:, :]
+    x_train, x_test = x[:train_test_split, :], x[train_test_split:, :]
     y = np.loadtxt(labelfile, dtype=int)
-    ytrain, ytest = y[:train_test_split], y[train_test_split:]
-    numSamples, numInputs = xtrain.shape
-    numOutputs = len(np.unique(ytrain))
+    y_train, y_test = y[:train_test_split], y[train_test_split:]
+    numSamples, numInputs = x_train.shape
+    maxOutput = np.max(y_train) + 1
+    t_train = to_categorical(y_train, maxOutput)  # convert output to categorical targets
+    t_test = to_categorical(y_test, maxOutput)
 
     pdb.set_trace()
 
     # Create the neural network
     model = Sequential()
     model.add(Input(numInputs,))
-    model.add(Dense(units=500, activation='relu', name='hidden1'))
-    model.add(Dense(units=500, activation='relu', name='hidden2'))
-    model.add(Dense(units=numOutputs, activation='softmax', name='output'))
+    model.add(Dense(units=100, activation='relu', name='hidden1'))
+    model.add(Dense(units=200, activation='relu', name='hidden2'))
+    model.add(Dense(units=100, activation='relu', name='hidden3'))
+    model.add(Dense(units=maxOutput, activation='softmax', name='output'))
     model.summary()
 
     input("Press <Enter> to train this network...")
 
     model.compile(
-        loss='mean_squared_error',
-        optimizer=SGD(learning_rate=0.001)
+        loss='categorical_crossentropy',
+        optimizer=SGD(learning_rate=0.001),
         metrics=['accuracy']
     )
 
@@ -53,13 +58,32 @@ def main():
     )
 
     # Train the network
-    history = model.fit(xtrain, ytrain,
-        batch_size=50,
-        epochs=40,
+    history = model.fit(x_train, t_train,
+        batch_size=10,
+        epochs=100,
         verbose=1,
         callbacks=[callback],
-        validation_data=(xtest, ytest)
+        validation_data=(x_test, t_test)
     )
+
+    # Compute the accuracy
+    metrics_train = model.evaluate(x_train, t_train, verbose=0)
+    print("=================================")
+    print(f"Training loss = {metrics_train[0]:0.4f}")
+    print(f"Training accuracy = {metrics_train[1]:0.4f}")
+    
+    metrics_test = model.evaluate(x_test, t_test, verbose=0)
+    print(f"Testing loss = {metrics_test[0]:0.4f}")
+    print(f"Testing accuracy = {metrics_test[1]:0.4f}")
+
+    # Plot the performance over time
+    plt.subplots()
+    plt.plot(history.history['accuracy'], label='training accuracy')
+    plt.plot(history.history['val_accuracy'], label='testing accuracy')
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend(loc='lower right')
+    plt.show()
 
 if __name__ == "__main__":
     main()
